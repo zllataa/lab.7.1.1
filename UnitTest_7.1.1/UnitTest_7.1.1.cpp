ba4eceaca87e4d8aa1a12d07733c2234050d40b7
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2022-02-23 23:06:55 +0200

    Add files via upload

---
+++ UnitTest_7.1.1/UnitTest_7.1.1.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest711
+{
+	TEST_CLASS(UnitTest711)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ lab.7.1/lab.7.1.cpp
@@ -0,0 +1,99 @@
+﻿// Lab_7_1.cpp
+// Половка Злата
+// Лабораторна робота No 7.1.
+// Пошук заданих елементів та впорядкування рядків / стовпчиків матриці.
+// Варіант 6
+
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+	const int High);
+void Print(int** a, const int rowCount, const int colCount);
+void Sort(int** a, const int rowCount, const int colCount);
+void Change(int** a, const int row1, const int row2, const int colCount);
+void Calc(int** a, const int rowCount, const int colCount, int& S, int& k);
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = 16;
+	int High = 97;
+	int rowCount = 8;
+	int colCount = 6;
+	int** a = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++)
+		a[i] = new int[colCount];
+	Create(a, rowCount, colCount, Low, High);
+	Print(a, rowCount, colCount);
+	Sort(a, rowCount, colCount);
+	Print(a, rowCount, colCount);
+	int S = 0;
+	int k = 0;
+	Calc(a, rowCount, colCount, S, k);
+	cout << "S = " << S << endl;
+	cout << "k = " << k << endl;
+	Print(a, rowCount, colCount);
+	for (int i = 0; i < rowCount; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+	const int High)
+{
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			a[i][j] = Low + rand() % (High - Low + 1);
+}
+void Print(int** a, const int rowCount, const int colCount)
+{
+	cout << endl;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+void Sort(int** a, const int rowCount, const int colCount)
+{
+	for (int i0 = 0; i0 < rowCount - 1; i0++)
+		for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+			if ((a[i1][0] < a[i1 + 1][0])
+				||
+				(a[i1][0] == a[i1 + 1][0] && a[i1][1] > a[i1 + 1][1])
+				||
+				(a[i1][0] == a[i1 + 1][0] &&
+					a[i1][1] == a[i1 + 1][1] &&
+					a[i1][2] < a[i1 + 1][3]))
+				Change(a, i1, i1 + 1, colCount);
+}
+void Change(int** a, const int row1, const int row2, const int colCount)
+{
+	int tmp;
+	for (int j = 0; j < colCount; j++)
+	{
+		tmp = a[row1][j];
+		a[row1][j] = a[row2][j];
+		a[row2][j] = tmp;
+	}
+}
+void Calc(int** a, const int rowCount, const int colCount, int& S, int& k)
+{
+	S = 0;
+	k = 0;
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			if (a[i][j] % 7 == 0 && ((!(i % 2 == 0)) || (!(j % 2 == 0))))
+			{
+				S += a[i][j];
+				k++;
+				a[i][j] = 0;
+			}
+}
